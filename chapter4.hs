c1d6fc3f71bb61bdd9e0247e4d3071cfb817fd78
lanocci <16540010+lanocci@users.noreply.github.com>
2021-08-29 19:48:49 +0900

    p43

--- chapter4.hs
+++ chapter4.hs
@@ -21,3 +21,37 @@ not True = False
 (&&) :: Bool -> Bool -> Bool
 True && b = b
 False && _ = False
+
+b && c | b == c = b
+       | otherwise = False
+
+-- tuple pattern
+fst :: (a, b) -> a
+fst (x, _) = x
+
+snd :: (a, b) -> b
+snd (_, y) = y
+
+
+-- list pattern
+test :: [Char] -> Bool
+test ['a',_,_] = True
+test _ = False
+
+-- cons 演算子 `:`
+-- [1,2,3]
+-- = 1 : [2,3]
+-- = 1 : 2 : [3]
+-- = 1 : 2 : 3 : []
+
+-- test関数は以下のように定義すると、任意の長さのリストに対応できる
+test2 :: [Char] -> Bool
+test2 ('a' : _) = True
+test2 _ = False
+
+head :: [a] -> a
+head (x : _) = x
+
+tail :: [a] -> [a]
+tail (_ : xs) = xs
+
